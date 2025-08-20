25d988b2b3b4fa28c0c3dc3ba0fe6a6ce95a3fcc
manvith22cs092-dev <manvith.22cs092@sode-edu.in>
2025-08-20 14:29:05 +0530

    Add files via upload

---
+++ cie.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    long long n, i, sum = 0;
+    clock_t start, end;
+    double cpu_time_used;
+
+    printf("Enter a positive number: ");
+    scanf("%lld", &n);
+
+    // Start measuring time
+    start = clock();
+
+    // Perform a simple computation (sum of first n numbers)
+    for (i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    // End measuring time
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Sum of first %lld numbers = %lld\n", n, sum);
+    printf("Execution time: %f seconds\n", cpu_time_used);
+
+    return 0;
+}
